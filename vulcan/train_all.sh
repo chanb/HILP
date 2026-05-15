fc929656772a019c3e737c3c5ab8f200adb8103a
Bryan Chan <bryan.chan@ualberta.ca>
2026-05-15 04:26:25 -0600

    Add vis

---
+++ vulcan/train_all.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --account=aip-schuurma
+#SBATCH --time=12:00:00
+#SBATCH --mem=64GB
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --array=1-1
+#SBATCH --output=/home/chanb/scratch/code/unsupervised_rl/logs/%j.out
+
+module load StdEnv/2023
+module load python/3.10.13
+module load cuda/12.9
+
+cd /home/chanb/scratch/code/unsupervised_rl/HILP/hilp_zsrl
+source .venv/bin/activate
+
+# FB REPRESENTATION
+# rnd
+PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=fb_ddpg agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=rnd load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/rnd/replay.pt replay_buffer_episodes=5000 experiment=rnd &
+
+# random
+PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=fb_ddpg agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=random load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/random/replay.pt replay_buffer_episodes=5000 experiment=random &
+
+# icm_apt
+PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=fb_ddpg agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=icm_apt load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/icm_apt/replay.pt replay_buffer_episodes=5000 experiment=icm_apt &
+
+# proto
+# PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=fb_ddpg agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=proto load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/proto/replay.pt replay_buffer_episodes=5000 experiment=proto &
+
+# HILP REPRESENTATION
+# rnd
+PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=sf agent.feature_learner=hilp p_randomgoal=0.375 agent.hilp_expectile=0.5 agent.hilp_discount=0.96 agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=rnd load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/rnd/replay.pt replay_buffer_episodes=5000 experiment=rnd &
+
+# random
+PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=sf agent.feature_learner=hilp p_randomgoal=0.375 agent.hilp_expectile=0.5 agent.hilp_discount=0.96 agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=random load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/random/replay.pt replay_buffer_episodes=5000 experiment=random &
+
+# icm_apt
+PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=sf agent.feature_learner=hilp p_randomgoal=0.375 agent.hilp_expectile=0.5 agent.hilp_discount=0.96 agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=icm_apt load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/icm_apt/replay.pt replay_buffer_episodes=5000 experiment=icm_apt &
+
+# proto
+PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=sf agent.feature_learner=hilp p_randomgoal=0.375 agent.hilp_expectile=0.5 agent.hilp_discount=0.96 agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=proto load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/proto/replay.pt replay_buffer_episodes=5000 experiment=proto &
+wait
--- vulcan/train_fb.sh
+++ vulcan/train_fb.sh
@@ -14,4 +14,17 @@ module load cuda/12.9
 cd /home/chanb/scratch/code/unsupervised_rl/HILP/hilp_zsrl
 source .venv/bin/activate
 
-PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=fb_ddpg agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=proto load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/proto/replay.pt replay_buffer_episodes=5000
+# FB REPRESENTATION
+# rnd
+PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=fb_ddpg agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=rnd load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/rnd/replay.pt replay_buffer_episodes=5000 &
+
+# random
+PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=fb_ddpg agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=random load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/random/replay.pt replay_buffer_episodes=5000 &
+
+# icm_apt
+PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=fb_ddpg agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=icm_apt load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/icm_apt/replay.pt replay_buffer_episodes=5000 &
+
+# proto
+# PYTHONPATH=. python url_benchmark/train_offline.py run_group=EXP device=cuda agent=fb_ddpg agent.q_loss=False seed=0 task=point_mass_maze_reach_bottom_right expl_agent=proto load_replay_buffer=/home/chanb/scratch/datasets/unsupervised_rl/datasets/point_mass_maze/proto/replay.pt replay_buffer_episodes=5000 &
+
+wait
